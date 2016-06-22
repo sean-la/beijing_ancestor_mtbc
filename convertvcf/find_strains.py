745766d80d417e859eefe96a1f503ac73318f9e1
Sean La <seanla1996@gmail.com>
2016-06-22 12:08:20 -0600

    Moved files around and found list of unique strains

--- convertvcf/find_strains.py
+++ convertvcf/find_strains.py
@@ -1,22 +1,27 @@
 sampleFilename = "sample_list.txt"
+redundantFileName = "redundant_samples.txt"
 strainFilename = "strain_list.txt"
 samples = 0
 strains = 0
 
+redundantStrains = []
+
+with open(redundantFileName, 'r') as file:
+	for line in file:
+		redundantStrains.append( line.rstrip() )	
+
 with open(sampleFilename, 'r') as input:
 	with open(strainFilename, 'w') as output:
-		previousSample = None
 		for line in input:
-			currentSample = int( line[3:].rstrip() )
+			currentSample = line.rstrip()
 			# If the current sample's ascension number is one more than the previous,
 			# we assume that the current sample comes from the same strain.
-			if previousSample is None or currentSample != previousSample + 1:
+			if currentSample not in redundantStrains:
 				print "Keeping sample  %s" % (line)
 				output.write(line)
 				strains += 1
 			else:
 				print "Skipping sample %s" % (line)
-			previousSample = currentSample	
 			samples += 1
 
 print "Number of strains: %d" %(strains)
