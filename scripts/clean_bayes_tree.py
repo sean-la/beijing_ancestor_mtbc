b51dfd5ce56e94ffd44f03d08e4c38fce1432e5c
Sean La <seanla1996@gmail.com>
2016-07-27 17:48:23 -0600

    Cleaned up dir

---
+++ scripts/clean_bayes_tree.py
@@ -0,0 +1,8 @@
+import sys
+import os
+import getopt
+import re
+
+pattern = "[&height=(.+),height_95%_HPD={(.+),(.+)},height_median=(.+),height_range={(.+),(.+)},length=(.+),length_95%_HPD={(.+),(.+)},length_median=(.+),length_range={(.+),(.+)}]:(.+)"
+
+
